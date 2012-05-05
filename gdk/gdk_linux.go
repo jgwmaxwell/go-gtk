8e853465b9b58f6c9a4878118091404da1547750
John Maxwell <jgwmaxwell@gmail.com>
2012-05-06 00:31:53 +0200

    Update gdk/gdk_linux.go

--- gdk/gdk_linux.go
+++ gdk/gdk_linux.go
@@ -4,7 +4,10 @@ package gdk
 #include <gdk/gdkx.h>
 // #cgo pkg-config: gdk-2.0 gthread-2.0
 */
-import "C"
+import (
+	"C"
+	"unsafe"
+)
 
 func (v *GdkWindow) GetNativeWindowID() int32 {
 	return int32(C.gdk_x11_drawable_get_xid((*C.GdkDrawable)(unsafe.Pointer(v.Window))))
